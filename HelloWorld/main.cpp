f43e3b15389b3cb2e56a4fb5a69d722c69eae042
Anthony Mihirana <mihids@gmail.com>
2018-07-09 16:45:23 +0530

    initial commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Anthony Mihirana on 9/7/18.
+//  Copyright © 2018 Anthony Mihirana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
